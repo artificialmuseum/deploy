e05e5e018c7b027d74e3cff51875a8693c9af9ae
jascha <jascha@jaeh.at>
2020-10-22 01:12:48 +0200

    bootstrap/pages.sh: loop over pages

--- bootstrap/pages.sh
+++ bootstrap/pages.sh
@@ -18,34 +18,50 @@ printf " - \033[0;32mdone\033[0m\n\n"
 
 ############################################################
 
+GIT_USER=thesystemcollective
 
-printf "\033[1;33mcloning page:\033[0m static.thesystem.at"
+PAGE_ARRAY=(
+  ("static.wiznwit.com" "$GIT_USER/static.thesystem.at")
+  ("glb.wiznwit.com" "$GIT_USER/glb.thesystem.at")
+)
 
-PAGE_HOST='static.wiznwit.com'
-DIR="/home/grundstein/repositories/$PAGE_HOST"
+# Loop and print it.  Using offset and length to extract values
+COUNT=${#PAGE_ARRAY[@]}
+for ((i=0; i<$COUNT; i++))
+do
+  PAGE_HOST=${!PAGE_ARRAY[i]:0:1}
+  PAGE_REPOSITORY=${!PAGE_ARRAY[i]:1:1}
 
-if [ ! -d "$DIR" ] ; then
-  git clone git://github.com/thesystemcollective/static.thesystem.at $DIR >> /var/log/grundstein/install.log 2>&1
-else
+  printf "\033[1;33mcloning:\033[0m $PAGE_REPOSITORY"
+
+  DIR="/home/grundstein/repositories/$PAGE_HOST"
+
+  if [ ! -d "$DIR" ] ; then
+    git clone --depth 1 git://github.com/$PAGE_REPOSITORY $DIR >> /var/log/grundstein/install.log 2>&1
+  else
+    cd "$DIR"
+    git pull origin master >> /var/log/grundstein/install.log 2>&1
+  fi
+
   cd "$DIR"
-  git pull origin master >> /var/log/grundstein/install.log 2>&1
-fi
 
-cd "$DIR"
+  # force update dependencies
+  rm -rf node_modules package-lock.json
 
-# force update dependencies
-rm -rf node_modules package-lock.json
+  # install dependencies
+  npm install --production >> /var/log/grundstein/install.log 2>&1
 
-# install dependencies
-npm install --production >> /var/log/grundstein/install.log 2>&1
+  # copy docs directory, if it exists
+  if [ -d "$DIR/docs" ]; then
+    mkdir -p /var/www/html/
+    cp -r ./docs /var/www/html/$PAGE_HOST
+  fi
 
-# copy docs directory, if it exists
-if [ -d "$DIR/docs" ]; then
-  mkdir -p /var/www/html/
-  cp -r ./docs /var/www/html/$PAGE_HOST
-fi
+  printf " - \033[0;32mdone\033[0m.\n\n"
+done
 
-printf " - \033[0;32mdone\033[0m.\n\n"
+
+############################################################
 
 
 ############################################################
