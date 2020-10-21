d5b11849a34cb40eed7547c4b3437868e21ef496
jascha <jascha@jaeh.at>
2020-10-22 00:11:09 +0200

    bootstrap/pages.sh: do not add grundstein.lock, do not write proxies multiple times

--- bootstrap/pages.sh
+++ bootstrap/pages.sh
@@ -2,24 +2,7 @@
 
 set -euf -o pipefail
 
-printf "\033[1;33mgrundstein\033[0m static page setup.\n"
-
-############################################################
-
-
-printf "\033[1;33mtouch\033[0m /grundstein.lock"
-
-if test -f "/grundstein.lock"; then
-  printf "/grundstein.lock exists.\n"
-  printf "there is an installation running or a past installation failed.\n"
-  printf "to force reinstallation, add the --force flag to the grundstein command.\n"
-  exit 1
-fi
-
-touch /grundstein.lock
-
-printf " - \033[0;32mdone\033[0m\n\n"
-
+printf "\033[1;33mgrundstein\033[0m static page setup.\n\n"
 
 ############################################################
 
@@ -36,7 +19,8 @@ printf " - \033[0;32mdone\033[0m\n\n"
 
 printf "\033[1;33mcloning page:\033[0m static.thesystem.at"
 
-DIR="/home/grundstein/repositories/static.wiznwit.com"
+PAGE_HOST='$PAGE_HOST'
+DIR="/home/grundstein/repositories/$PAGE_HOST"
 
 if [ ! -d "$DIR" ] ; then
   git clone git://github.com/thesystemcollective/static.thesystem.at $DIR >> /var/log/grundstein/install.log 2>&1
@@ -56,7 +40,7 @@ npm install >> /var/log/grundstein/install.log 2>&1
 # copy docs directory, if it exists
 if [ -d "$DIR/docs" ]; then
   mkdir -p /var/www/html/
-  cp -r ./docs /var/www/html/static.wiznwit.com
+  cp -r ./docs /var/www/html/$PAGE_HOST
 fi
 
 printf " - \033[0;32mdone\033[0m.\n\n"
@@ -73,25 +57,13 @@ if [ ! -f "$DIR/docs" ]; then
   touch $PROXY_FILE
 fi
 
-HAS_PROXY=grep -qxF 'static.wiznwit.com' $PROXY_FILE
-if [ $? != 0 ]; then
-  echo 'static.wiznwit.com' >> $PROXY_FILE
-fi
+grep -qxF "$PAGE_HOST" $PROXY_FILE || echo "$PAGE_HOST" >> $PROXY_FILE
 
 printf " - \033[0;32mdone\033[0m\n\n"
 
 
 ############################################################
 
-
-printf "\033[1;33mremoving /grundstein.lock\033[0m"
-
-rm -f /grundstein.lock
-
-printf " - \033[0;32mdone\033[0m\n\n"
-
-echo "\033[1;33mGRUNDSTEIN\033[0m - static page install finished." >> /var/log/grundstein/install.log
-
-printf "STATIC PAGE INSTALL FINISHED.\n"
+printf "\033[1;33mpages.sh\033[0m - \033[0;32mdone\033[0m\n\n"
 
 ############################################################
