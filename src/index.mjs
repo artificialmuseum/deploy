1114582652cb45c06645b3a6c08e6222e74e138b
jaeh <jascha@jaeh.at>
2021-02-01 00:07:49 +0100

    src/index.mjs: add SSH_USER_NAME env arg

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@ import path from 'path'
 
 import cli from '@magic/cli'
 
-import { fs } from '@grundstein/commons'
+import { fs, log } from '@grundstein/commons'
 
 import sh from './sh/index.mjs'
 
@@ -22,7 +22,9 @@ set -euf -o pipefail
   
 export DEBIAN_FRONTEND=noninteractive
 
-###############################3
+sudo su
+
+###############################
 `.trim()
 
   const init = await sh.init(args)
@@ -45,12 +47,19 @@ export DEBIAN_FRONTEND=noninteractive
   ].filter(a => a).join('')
 
   //   const exec = `
-  // ssh root@${args.ip} bash -s < bootstrap/init.sh
+  // ssh ${SSH_USER_NAME}@${args.ip} bash -s < bootstrap/init.sh
   // `.trim()
 
   const outputFile = path.join(cwd, 'bootstrap', 'remote.sh')
 
   await fs.writeFile(outputFile, result)
+
+  const { SSH_USER_NAME } = process.env
+
+  if (!SSH_USER_NAME) {
+    log.error('No SSH_USER_NAME process.env variable found')
+    process.exit(1)
+  }
 
   const deploy = `
 #!/usr/bin/env bash
@@ -60,12 +69,13 @@ set -euf -o pipefail
 printf "${YELLOW}GRUNDSTEIN${NC} - pushing to production.\n\n"
 
 if test -f ".secrets/digitalocean.ini"; then
-  ssh root@${args.ip} 'mkdir -p /.secrets'
-  scp .secrets/digitalocean.ini root@${args.ip}:/.secrets/digitalocean.ini
-  ssh root@${args.ip} 'chmod 600 /.secrets/digitalocean.ini'
+  ssh ${SSH_USER_NAME}@${args.ip} 'mkdir -p /.secrets'
+  scp .secrets/digitalocean.ini ${SSH_USER_NAME}@${args.ip}:/home/${SSH_USER_NAME}/digitalocean.ini
+  ssh -t ${SSH_USER_NAME}@${args.ip} 'sudo mv /home/${SSH_USER_NAME}/digitalocean.ini /.secrets/digitalocean.ini'
+  ssh ${SSH_USER_NAME}@${args.ip} 'chmod 600 /.secrets/digitalocean.ini'
 fi
 
-ssh root@${args.ip} bash -s < bootstrap/remote.sh
+ssh ${SSH_USER_NAME}@${args.ip} bash -s < bootstrap/remote.sh
 
 `.trim()
 
